e17baea24b552d12a7d19c3189a1139b298fee83
Franchesco <franciscocastano.pdf@gmail.com>
2025-12-16 16:44:06 +0100

    Bayeta de la fortuna

---
+++ ENTORNO CLIENTE/2EV/ANGULAR/Proyecto01/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { VideojuegoComponent } from './components/videojuego-component/videojuego-component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    VideojuegoComponent
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Proyecto01');
+}
